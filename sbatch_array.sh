a14b13d3f010500864196dcc7fe4835b8217eca8
yianchen <r08222011@gmail.com>
2023-11-17 18:52:27 +0800

    20231117 Add sbatch_array

---
+++ sbatch_array.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --array=0-2
+#SBATCH --exclusive
+
+pwd
+python_version="$(python --version)"
+required_version="Python 3.9.12"
+echo current python version is $python_version
+echo required python version is $required_version
+
+if [ "$python_version" = "$required_version" ]; then
+    echo "Python 3.9.12 detected"
+    echo $SLURM_ARRAY_TASK_ID
+    python a.py --rnd_seed $SLURM_ARRAY_TASK_ID
+else
+    echo "Python 3.9.12 not detected"
+fi
